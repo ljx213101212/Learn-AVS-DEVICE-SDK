ddd508d9856138c11c7c6a21955e82587710d262
ljx213101212 <lijixiang.nus@gmail.com>
2019-06-05 15:01:51 +0800

    add include additional header https://docs.microsoft.com/en-us/cpp/build/reference/vcpp-directories-property-page?f1url=https%3A%2F%2Fmsdn.microsoft.com%2Fquery%2Fdev15.query%3FappId%3DDev15IDEF1%26l%3DEN-US%26k%3Dk(VS.ToolsOptionsPages.Projects.VCDirectories)%26rd%3Dtrue&view=vs-2019

---
+++ avs-device-sdk/SampleApp/SampleApp/main.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+
+#include "SampleApp\ConsoleReader.h"
+#include "SampleApp/SampleApplication.h"
+#include "SampleApp/SampleApplicationReturnCodes.h"
+#include <cstdlib>
+#include <string>
+
+
+
+
+int main(int argc, char* argv[])
+{
+	std::vector<std::string> configFiles;
+	std::string pathToKWDInputFolder;
+	std::string logLevel;
+
+
+	return 0;
+}
